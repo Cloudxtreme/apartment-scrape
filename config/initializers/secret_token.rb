78646d7985dbd1ed8f53cfb629f0ca051e4005a5
David Cuddeback <david.cuddeback@gmail.com>
2010-12-18 19:07:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apartments::Application.config.secret_token = 'a74452853da959d759291e4ecdaa03260e4cd490afcf3b554e8c9fb8a036b6063084bb295d181796b80bb4a15817af28e410544a0b9daf7ed624fe9e767a1ee6'
